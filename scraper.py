3f328deb032ec39ca04d4acc914e0eb904720563
Derek Willis <dwillis@gmail.com>
2015-09-27 21:17:00 -0400

    added initial parish-level scraper

---
+++ scraper.py
@@ -0,0 +1,61 @@
+import unicodecsv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+headers = ['parish', 'office', 'district', 'party', 'candidate', 'votes']
+OFFICE_LIST = ['Lieutenant Governor', 'U. S. Senator', 'U. S. Representative', 'Attorney General', 'Governor', 'Presidential Electors', 'State Senator', 'State Representative', 'Secretary of State', 'Commissioner of']
+
+def write_parish_results(url, file_name):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    offices = [x.text for x in soup.findAll('strong') if x.text.split(' -- ')[0].strip() in OFFICE_LIST]
+    parish_details = get_parish_details(url, soup, offices)
+    candidate_details = get_candidate_details(soup, offices)
+    scrape_parish_results(parish_details, candidate_details, file_name)
+
+
+def get_candidate_details(soup, offices):
+    candidate_details = []
+    combined = zip(offices, soup.findAll('table'))
+    for office, table in combined:
+        rows = table.findAll('tr')[1:]
+        for row in rows:
+            candidate_row = [x.text for x in row.findAll('td')]
+            candidate_row.append(candidate_row[0].split('(')[1].replace(')',''))
+            candidate_row[0] = candidate_row[0].split('(')[0].strip()
+            candidate_row.append(office)
+            candidate_details.append(candidate_row)
+    return candidate_details
+
+def get_parish_details(url, soup, offices):
+    parish_links = ["http://"+url.split('/')[2]+"/"+url.split('/')[3]+'/'+x['href'] for x in soup.findAll('a') if "Parish" in x.text]
+    return zip(offices, parish_links)
+
+def scrape_parish_results(parish_details, candidate_details, file_name):
+    precinct_links = {}
+    with open(file_name, 'wb') as csvfile:
+        w = unicodecsv.writer(csvfile, encoding='utf-8')
+        w.writerow(headers)
+        for office, url in parish_details:
+            if ' -- ' in office:
+                office_name, district = office.split(' -- ')
+                district = district.strip()
+            else:
+                office_name = office
+                district = None
+            if office_name == 'Commissioner of':
+                office_name = office_name + ' ' + district
+                district = None
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            precinct_links[office] = ["http://"+url.split('/')[2]+"/"+url.split('/')[3]+'/'+x['href'] for x in soup.findAll('a') if "Precinct" in x.text]
+            rows = soup.findAll('tr')
+            candidates_for_office = [x.text for x in rows[0] if x.text in [c[0] for c in candidate_details]]
+            for row in rows[1:]:
+                parish_name = row.find('strong').text
+                vote_totals = [int(x.text) for x in row.findAll('td')[1:]]
+                for candidate, votes in zip(candidates_for_office, vote_totals):
+                    party = [c[3] for c in candidate_details if candidate == c[0]][0]
+                    w.writerow([parish_name, office_name, district, party, candidate, votes])
+
+    return precinct_links
