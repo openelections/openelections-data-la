8640b154e2e77bef0f7888d11b05bbf368d8851b
Derek Willis <dwillis@gmail.com>
2015-09-30 10:00:01 -0400

    cleaned up and added imports

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import unicodecsv
 import requests
+import metadata
 from BeautifulSoup import BeautifulSoup
 
 headers = ['parish', 'office', 'district', 'party', 'candidate', 'votes']
@@ -17,8 +18,8 @@ def write_parish_results(url, file_name):
     offices = [x.text for x in soup.findAll('strong') if x.text.split(' -- ')[0].strip() in OFFICE_LIST]
     parish_details = get_parish_details(url, soup, offices)
     candidate_details = get_candidate_details(soup, offices)
-    scrape_parish_results(parish_details, candidate_details, file_name)
-
+    precinct_links = scrape_parish_results(parish_details, candidate_details, file_name + '.csv')
+    # write_precinct_results(zip(offices,precinct_links), candidate_details, file_name + '__precinct.csv')
 
 def get_candidate_details(soup, offices):
     candidate_details = []
@@ -65,3 +66,28 @@ def scrape_parish_results(parish_details, candidate_details, file_name):
                     w.writerow([parish_name, OFFICE_LOOKUP[office_name], district, party, candidate, votes])
 
     return precinct_links
+
+def write_precinct_results(precinct_details, candidate_details, file_name):
+    with open(file_name, 'wb') as csvfile:
+        w = unicodecsv.writer(csvfile, encoding='utf-8')
+        w.writerow(headers)
+        for office, url in precinct_details:
+            if ' -- ' in office:
+                office_name, district = office.split(' -- ')
+                district = district.strip()
+            else:
+                office_name = office
+                district = None
+            if office_name == 'Commissioner of':
+                office_name = office_name + ' ' + district
+                district = None
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            rows = soup.findAll('tr')
+            candidates_for_office = [x.text for x in rows[0] if x.text in [c[0] for c in candidate_details]]
+            for row in rows[1:]:
+                precinct_name = row.find('strong').text
+                vote_totals = [int(x.text) for x in row.findAll('td')[1:]]
+                for candidate, votes in zip(candidates_for_office, vote_totals):
+                    party = [c[3] for c in candidate_details if candidate == c[0]][0]
+                    w.writerow([precinct_name, OFFICE_LOOKUP[office_name], district, party, candidate, votes])
