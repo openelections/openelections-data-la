87c5c7bcff7985312fab3d2a229657ac292d958d
Derek Willis <dwillis@gmail.com>
2019-02-10 11:13:23 -0500

    updated excel parser for 2018 general

--- excel_parser.py
+++ excel_parser.py
@@ -2,55 +2,73 @@ import xlrd
 import unicodecsv
 
 headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes']
-book = xlrd.open_workbook("/Users/dwillis/Downloads/Election+Results+(11-08-2016).xlsx")
+book = xlrd.open_workbook("/Users/dwillis/Downloads/Election+Results+(11-06-2018).xlsx")
 sheet = book.sheet_by_index(1)
-president_candidates = [x.value for x in sheet.row(6) if x.value != ''] # row - 1
-president_rows = range(7,4104)
-senate_candidates = [x.value for x in sheet.row(4107) if x.value != '']
-senate_rows = range(4108,8205)
-house1_candidates = [x.value for x in sheet.row(8208) if x.value != '']
-house1_rows = range(8209,8775)
-house2_candidates = [x.value for x in sheet.row(8778) if x.value != '']
-house2_rows = range(8779,9466)
-house3_candidates = [x.value for x in sheet.row(9469) if x.value != '']
-house3_rows = range(9470,10068)
-house4_candidates = [x.value for x in sheet.row(10071) if x.value != '']
-house4_rows = range(10072,10868)
-house5_candidates = [x.value for x in sheet.row(10871) if x.value != '']
-house5_rows = range(10872,11769)
-house6_candidates = [x.value for x in sheet.row(11772) if x.value != '']
-house6_rows = range(11773,12379)
+#president_candidates = [x.value for x in sheet.row(6) if x.value != ''] # row - 1
+#president_rows = range(7,4104)
+sos_candidates = [x.value for x in sheet.row(6) if x.value != '']
+sos_rows = range(7,4045)
+house1_candidates = [x.value for x in sheet.row(4049) if x.value != '']
+house1_rows = range(4050,4619)
+house2_candidates = [x.value for x in sheet.row(4623) if x.value != '']
+house2_rows = range(4624,5309)
+house3_candidates = [x.value for x in sheet.row(5313) if x.value != '']
+house3_rows = range(5314,5911)
+house4_candidates = [x.value for x in sheet.row(5915) if x.value != '']
+house4_rows = range(5916,6711)
+house5_candidates = [x.value for x in sheet.row(6715) if x.value != '']
+house5_rows = range(6716,7614)
+house6_candidates = [x.value for x in sheet.row(7618) if x.value != '']
+house6_rows = range(7619,8225)
+state_senate26_candidates = [x.value for x in sheet.row(8630) if x.value != '']
+state_senate26_rows = range(8631,8740)
+state_rep33_candidates = [x.value for x in sheet.row(8743) if x.value != '']
+state_rep33_rows = range(8744,8775)
+state_rep90_candidates = [x.value for x in sheet.row(8778) if x.value != '']
+state_rep90_rows = range(8779,8810)
 
-
-with open('20161108__la__general__precinct.csv', 'wb') as csvfile:
+with open('20181106__la__general__precinct.csv', 'wb') as csvfile:
     w = unicodecsv.writer(csvfile, encoding='utf-8')
     w.writerow(headers)
+#    county = None
+#    for row in president_rows:
+#        pres_values = [x.value for x in sheet.row(row) if x.value !='']
+#        if len(pres_values) == 1:
+#            county = pres_values[0]
+#        else:
+#            precinct = pres_values[0]
+#            cand_votes = zip(president_candidates, pres_values[1:])
+#            for cand in cand_votes:
+#                name, votes = cand
+#                name, party = name.split(', ', 1)
+#                party = party.split(' (')[1].replace(')','')
+#                w.writerow([county, precinct, 'President', None, party, name, int(votes)])
+#    county = None
+#    for row in senate_rows:
+#        senate_values = [x.value for x in sheet.row(row) if x.value !='']
+#        if len(senate_values) == 1:
+#            county = senate_values[0]
+#        else:
+#            precinct = senate_values[0]
+#            cand_votes = zip(senate_candidates, senate_values[1:])
+#            for cand in cand_votes:
+#                name, votes = cand
+#                name, party = name.split(' (')
+#                party = party.replace(')','')
+#                w.writerow([county, precinct, 'U.S. Senate', None, party, name, int(votes)])
     county = None
-    for row in president_rows:
-        pres_values = [x.value for x in sheet.row(row) if x.value !='']
-        if len(pres_values) == 1:
-            county = pres_values[0]
+    for row in sos_rows:
+        sos_values = [x.value for x in sheet.row(row) if x.value !='']
+        if len(sos_values) == 1:
+            county = sos_values[0]
         else:
-            precinct = pres_values[0]
-            cand_votes = zip(president_candidates, pres_values[1:])
-            for cand in cand_votes:
-                name, votes = cand
-                name, party = name.split(', ', 1)
-                party = party.split(' (')[1].replace(')','')
-                w.writerow([county, precinct, 'President', None, party, name, int(votes)])
-    county = None
-    for row in senate_rows:
-        senate_values = [x.value for x in sheet.row(row) if x.value !='']
-        if len(senate_values) == 1:
-            county = senate_values[0]
-        else:
-            precinct = senate_values[0]
-            cand_votes = zip(senate_candidates, senate_values[1:])
+            precinct = sos_values[0]
+            cand_votes = zip(sos_candidates, sos_values[1:])
             for cand in cand_votes:
                 name, votes = cand
                 name, party = name.split(' (')
                 party = party.replace(')','')
-                w.writerow([county, precinct, 'U.S. Senate', None, party, name, int(votes)])
+                w.writerow([county, precinct, 'Secretary of State', None, party, name, int(votes)])
     county = None
     for row in house1_rows:
         house1_values = [x.value for x in sheet.row(row) if x.value !='']
@@ -129,3 +147,42 @@ with open('20161108__la__general__precinct.csv', 'wb') as csvfile:
                 name, party = name.split(' (')
                 party = party.replace(')','')
                 w.writerow([county, precinct, 'U.S. House', 6, party, name, int(votes)])
+    county = None
+    for row in state_senate26_rows:
+        state_senate26_values = [x.value for x in sheet.row(row) if x.value !='']
+        if len(state_senate26_values) == 1:
+            county = state_senate26_values[0]
+        else:
+            precinct = state_senate26_values[0]
+            cand_votes = zip(state_senate26_candidates, house6_values[1:])
+            for cand in cand_votes:
+                name, votes = cand
+                name, party = name.split(' (')
+                party = party.replace(')','')
+                w.writerow([county, precinct, 'State Senate', 26, party, name, int(votes)])
+    county = None
+    for row in state_rep33_rows:
+        state_rep33_values = [x.value for x in sheet.row(row) if x.value !='']
+        if len(state_rep33_values) == 1:
+            county = state_rep33_values[0]
+        else:
+            precinct = state_rep33_values[0]
+            cand_votes = zip(state_rep33_candidates, house6_values[1:])
+            for cand in cand_votes:
+                name, votes = cand
+                name, party = name.split(' (')
+                party = party.replace(')','')
+                w.writerow([county, precinct, 'State House', 33, party, name, int(votes)])
+    county = None
+    for row in state_rep90_rows:
+        state_rep90_values = [x.value for x in sheet.row(row) if x.value !='']
+        if len(state_rep90_values) == 1:
+            county = state_rep90_values[0]
+        else:
+            precinct = state_rep90_values[0]
+            cand_votes = zip(state_rep90_candidates, house6_values[1:])
+            for cand in cand_votes:
+                name, votes = cand
+                name, party = name.split(' (')
+                party = party.replace(')','')
+                w.writerow([county, precinct, 'State House', 90, party, name, int(votes)])
